aafe5049b91f0ee845675d69d39d80119be270d5
ZamanovRenat <zamanov.renat@mail.ru>
2022-06-02 16:28:07 +0300

    Модель пиццы

---
+++ PizzaWebApplication/Models/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaWebApplication.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Тесто: тонкое || традиционное
+        /// </summary>
+        public string Dough { get; set; }
+        /// <summary>
+        /// Диаметр пиццы
+        /// </summary>
+        public int Diam { get; set; }
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// Фото пиццы
+        /// </summary>
+        public string ImageUrl { get; set; }
+    }
+}
