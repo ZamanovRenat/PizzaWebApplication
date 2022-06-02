06fcc927c46d280f5b835e0a6db7292232c384d4
ZamanovRenat <zamanov.renat@mail.ru>
2022-06-02 17:16:06 +0300

    Класс контекста БД

---
+++ PizzaWebApplication/Data/PizzaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaWebApplication.Models;
+
+namespace PizzaWebApplication.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public PizzaContext(DbContextOptions<PizzaContext> dbContextOptions) : base(dbContextOptions)
+        {
+            //Пересоздание БД
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Pizzas.db");
+        }
+    }
+}
